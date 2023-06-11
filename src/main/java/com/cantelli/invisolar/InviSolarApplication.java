1da38401e84f9c0feac8aa53af7daea740829ebd
Andrea Cantelli <andrea.cantelli@edu.unife.it>
2023-06-11 18:04:15 +0200

    pom and application prop

---
+++ src/main/java/com/cantelli/invisolar/InviSolarApplication.java
@@ -0,0 +1,13 @@
+package com.cantelli.invisolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InviSolarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InviSolarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cantelli/invisolar/InviSolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cantelli.invisolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InviSolarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
